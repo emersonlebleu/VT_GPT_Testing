d14b5c2a23be3e40d095fed1e5c8982f0d125ce4
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-05-25 13:45:05 -0600

    Clean & Comment AI
    Cleaned up and commented the AI class and functions/methods.

--- ai.py
+++ ai.py
@@ -10,7 +10,7 @@ class Ai:
         if text == '':
             return 'No text was provided.'
 
-        prompt5 = f'''Dear GPT, I need you to help with processing the survey text. Your task is to extract all the questions from this text and structure them in a specific way. When a question is followed by related sub-questions, treat them as a unit. Organize these units in a list of lists. For example, the text 'Please describe your PI: Name, Age, Years. How many of the following patients do you see each year? MM, AML, BMT' should be interpreted as: [['Please describe your PI:', 'Name', 'Age', 'Years'], ['How many of the following patients do you see each year?', 'MM', 'AML', 'BMT']].
+        prompt = f'''Dear GPT, I need you to help with processing the survey text. Your task is to extract all the questions from this text and structure them in a specific way. When a question is followed by related sub-questions, treat them as a unit. Organize these units in a list of lists. For example, the text 'Please describe your PI: Name, Age, Years. How many of the following patients do you see each year? MM, AML, BMT' should be interpreted as: [['Please describe your PI:', 'Name', 'Age', 'Years'], ['How many of the following patients do you see each year?', 'MM', 'AML', 'BMT']].
             Now, apply this method to the following survey text: {text}. Ensure that each question string is between single quotation marks as demonstrated earlier.'''
 
         openai.api_key = API_KEY
@@ -19,7 +19,7 @@ class Ai:
                 model=model,
                 messages=[
                     {"role": "system", "content": "You are an AI that identifies the questions or actionable text from the text within questionnaires and returns them in a properly formatted python list."},
-                    {"role": "user", "content": prompt5}
+                    {"role": "user", "content": prompt}
                 ]
             )
         except:
@@ -35,7 +35,7 @@ class Ai:
                     model=model,
                     messages=[
                         {"role": "system", "content": "You are an AI that identifies the questions or actionable text from the text within questionnaires and returns them in a properly formatted python list."},
-                        {"role": "user", "content": prompt5}
+                        {"role": "user", "content": prompt}
                     ]
                 )
             except:
@@ -48,18 +48,25 @@ class Ai:
         return text_response
 
 def standardize_text(text):
-    #make sure parentheses are balanced in the string if not add or remove parentheses as necessary
+    '''
+    Takes in a string from open AI and ensures the format doesn't have errors typical for the model's response
+    returns a string with the proper formatting to be directly converted into a python list of lists.
+    '''
+
+    #Variables
     opens = 0
     closes = 0
     special =['[', ']', ',', ' ']
     corrected_text = ''
     watching = False
-    last_location = 0
-    #remove any newlines
+
+    #Remove any newlines
     text = text.replace('\n', '')
-    #remove any multiple spaces replace with single space
+
+    #Remove any multiple spaces replace with single space
     text = ' '.join(text.split())
 
+    #Main loop
     for i, char in enumerate(text):
         if not watching: 
             if char == special[0]:
@@ -72,21 +79,24 @@ def standardize_text(text):
             if char == special[0]:
                 #find the last non special character and make sure the format is correct
                 for j in range(len(corrected_text)-1, 0, -1):
+                    #if we find a close it will be removed so decrement closes
                     if corrected_text[j] == special[1]:
                         closes -= 1
+                    #When the next non special character is found add a close, comma, and space
                     if corrected_text[j] not in special:
                         corrected_text = corrected_text[:j+1] + '], '
                         corrected_text += char
                         opens += 1
                         closes += 1
                         break
-                #turn off watching
                 watching = False
             else:
+                #If you don't find an open just continue as usual
                 corrected_text += char
                 if char == special[1]:
                     closes += 1
-    #ensure that opens matches closes if it doesnt add the appropriate number of closes
+
+    #Ensure that opens matches closes if it doesnt add or remove the appropriate number of closes
     if opens > closes:
         for i in range(opens-closes):
             corrected_text += ']'
